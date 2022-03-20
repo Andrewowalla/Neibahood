cbf5145031f3fc43fbdbd9439a35ff9db10c3787
Andrewowalla <andrewowalla@gmail.com>
2022-03-20 17:07:33 +0300

    Create  model class profile

--- neiba/models.py
+++ neiba/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
-    # admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
     logo = CloudinaryField("image")
     description = models.TextField()
     health_no = models.IntegerField(null=True, blank=True)
@@ -23,3 +26,24 @@ class NeighbourHood(models.Model):
     @classmethod
     def find_neighborhood(cls, neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
+
+class Profile(models.Model):
+    """Class model for user profile"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = CloudinaryField("image")
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'   
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
