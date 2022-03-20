541611a2890765e092f7a0fe178fc6ba92cf56d0
Andrewowalla <andrewowalla@gmail.com>
2022-03-20 18:23:51 +0300

    urls for profile and neighbourhood

--- neiba/urls.py
+++ neiba/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('', views.hoods, name='hoods'),
     path('account/', include('django.contrib.auth.urls')),
     path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+   
 ]
