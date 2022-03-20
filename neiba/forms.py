76b0ecf6c4c655c858bbf80d3115a47a70a6a390
Andrewowalla <andrewowalla@gmail.com>
2022-03-20 17:51:03 +0300

    Create PostForm, Updateform and BusinessForm

--- neiba/forms.py
+++ neiba/forms.py
@@ -15,4 +15,17 @@ class NeighbourHoodForm(forms.ModelForm):
         model = NeighbourHood
         exclude = ('admin',)     
 
-    
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')   
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'neighbourhood')
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('admin',)     
