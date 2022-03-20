2e78d220c64172f4c7f83134d19c11b81fd0102f
Andrewowalla <andrewowalla@gmail.com>
2022-03-20 17:28:50 +0300

    Register model classes Profile, Business and Post to admin.py

--- neiba/admin.py
+++ neiba/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import NeighbourHood
+from .models import NeighbourHood, Profile, Business, Post
 
 admin.site.register(NeighbourHood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+
