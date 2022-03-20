bad80eeb3f4b790887735858ed811f9d0d2053c2
Andrewowalla <andrewowalla@gmail.com>
2022-03-20 18:01:29 +0300

    Add urls for login, logout and register

--- neiba/urls.py
+++ neiba/urls.py
@@ -3,5 +3,9 @@ from .import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
     path('', views.hoods, name='hoods'),
+    
 ]
